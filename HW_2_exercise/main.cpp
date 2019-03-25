47e495f432c19401cb74b2848d92d68eb72efdfd
Justin Grabowski <justingrabowski@yahoo.com>
2019-03-25 14:03:25 -0400

    Initial Commit

---
+++ HW_2_exercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW_2_exercise
+//
+//  Created by Justin Grabowski on 3/25/19.
+//  Copyright © 2019 Justin Grabowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
